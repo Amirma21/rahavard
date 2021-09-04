c3d5f4045a9ac15d65851ebcc97698f27e37121e
amir maleki <amirmaleki21physics@gmail.com>
2021-09-04 16:20:12 +0430

    progress bar added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
+//!preloader
+
 window.addEventListener("load", ()=>{
     const preloader = document.querySelector(".preloader")
     preloader.classList.add("hidepreloader")
 })
+
+//!progressbar
+
+const progress = document.querySelector('.progress-show');
+console.log(progress);
+
+
+window.addEventListener("scroll", ()=>{  
+    const winscroll = window.pageYOffset;
+
+    const height = document.documentElement.scrollHeight - window.innerHeight;
+    
+    const scrolled = (winscroll / height) * 100;
+    
+    progress.style.width = `${scrolled}%`;})
+
+
+
+
 
 
 
